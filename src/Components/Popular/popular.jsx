adf7d00500df18c1e4faa662b4c5730755d003a2
Prajeet Shah <prajeetshah93@gmail.com>
2024-01-22 18:50:50 +0545

    item popular

--- src/Components/Popular/popular.jsx
+++ src/Components/Popular/popular.jsx
@@ -3,10 +3,10 @@ import data_product from '../Assets/data';
 import Item from '../item/Item';
 const popular = () => {
   return (
-    <div className='popular'>
-      <h1>POPULAR IN WOMEN</h1>
-      <hr/>
-      <div className='popular-item'>
+    <div className='popular flex flex-col items-center gap-4  '>
+      <h1 className='text-black text-16 font-medium'>POPULAR IN WOMEN</h1>
+      <hr className='w-48 h-2 rounded-lg bg-brown-200' />
+      <div className='popular-item mt-16 flex gap-10'>
         {data_product.map((item , i)=>{
             return <Item key = {i} id = {item.id} name = {item.name} 
             image = {item.image} new_price = {item.new_price} old_price = {item.old_price} /> 
--- src/Components/item/Item.jsx
+++ src/Components/item/Item.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Item = (props) => {
   return (
-    <div className='item w-60 hover:transform scale-105 transition-700'>
-      <img src={props.image} />
+    <div className='item w-60  '>
+      <img className='transition ease-in-out hover:scale-105 rounded-md' src={props.image} />
       <p className='mx-2 my-0'>{props.name}</p>
       <div className='item-prices flex gap-8 '>
         <div className='item-price-new text-gray-600 text-lg font-medium'>
-            {props.new_price}
+            ${props.new_price}
         </div>
         <div className='item-price-old text-gray-400 text-lg font-normal line-through'>
-            {props.old_price}
+            ${props.old_price}
         </div>
       </div>
     </div>
