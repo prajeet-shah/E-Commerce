4a333fc3405601ba44f47d5c819e0d79fdeb18ba
Prajeet Shah <prajeetshah93@gmail.com>
2024-01-23 20:00:25 +0545

    offers

---
+++ src/Components/Offers/Offers.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Offers = () => {
+  return (
+    <div className='offers'>
+      <div className='offers-left'>
+        <h1>Exclusive</h1>
+        <h1>Offers For You</h1>
+      </div>
+      <div className='offers-right'></div>
+    </div>
+  )
+}
+
+export default Offers
