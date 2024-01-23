cc8efee2de3410c5eb3ce68b474923a6a6734fe4
Prajeet Shah <prajeetshah93@gmail.com>
2024-01-23 21:28:54 +0545

    change in offer

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-
+import exclusive_image from '../Assets/exclusive_image.png';
 const Offers = () => {
   return (
-    <div className='offers'>
-      <div className='offers-left'>
-        <h1>Exclusive</h1>
-        <h1>Offers For You</h1>
+    <div className='offers w-4/6 h-60vh flex mx-auto my-auto mt-20 py-0 px-32 bg-gradient-to-t from-white to-pink-100'>
+      <div className='offers-left flex-1 flex flex-col justify-center'>
+        <h1 className='text-black text-3xl'>Exclusive</h1>
+        <h1 className='text-black text-3xl'>Offers For You</h1>
+        <p className='text-black text-lg font-medium'>ONLY ON BEST SELLERS PRODUCTS</p>
+        <button className='w-44 h-10 bg-red-600 border-none text-white text-lg font-medium rounded-3xl mt-12 cursor-pointer'>Check Now</button>
       </div>
-      <div className='offers-right'></div>
+      <div className='offers-right'>
+        <img src={exclusive_image}/>
+      </div>
     </div>
   )
 }
--- src/Components/Popular/popular.jsx
+++ src/Components/Popular/popular.jsx
@@ -3,10 +3,10 @@ import data_product from '../Assets/data';
 import Item from '../item/Item';
 const popular = () => {
   return (
-    <div className='popular flex flex-col items-center gap-4  '>
-      <h1 className='text-black text-16 font-medium'>POPULAR IN WOMEN</h1>
+    <div className='popular flex flex-col items-center gap-4 mt-32 '>
+      <h1 className='text-black text-2xl font-semibold'>POPULAR IN WOMEN</h1>
       <hr className='w-48 h-2 rounded-lg bg-brown-200' />
-      <div className='popular-item mt-16 flex gap-10'>
+      <div className='popular-item mt-6 flex gap-10'>
         {data_product.map((item , i)=>{
             return <Item key = {i} id = {item.id} name = {item.name} 
             image = {item.image} new_price = {item.new_price} old_price = {item.old_price} /> 
